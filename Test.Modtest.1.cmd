deaaeb12e3843b1812afff94362ddd14ab8746d4
Achim Kuntz <achimobil@hotmail.com>
2024-01-31 16:49:41 +0100

    cmd

--- Test.Modtest.1.cmd
+++ Test.Modtest.1.cmd
@@ -1,3 +1,3 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_BigDisplay.zip" -x@FileDefinition.txt
-"C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 1 -cheats -profile E:\LS22Profile\LS22ModTest -devWarnings
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_BigDisplay.zip" -x@FileDefinition.txt
+start "" "C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 1 -cheats -profile E:\LS22Profile\LS22ModTest -devWarnings
 
